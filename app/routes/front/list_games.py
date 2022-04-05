ecae2a0b22114bea4af505d8b4ca56019e3c5382
Damien "Tina" Savatier <damien.savatier@epitech.eu>
2022-04-05 09:32:47 +0200

    Add search

--- app/routes/front/list_games.py
+++ app/routes/front/list_games.py
@@ -8,17 +8,27 @@ from . import bp
 LIMIT = 10
 
 
+def get_games_query(**kwargs):
+    search = flask.request.args.get('q')
+    query = db.session.query(Game)
+    if search:
+        query = query.filter(Game.name.ilike(f'%{search}%'))
+    for method, argument in kwargs.items():
+        query = getattr(query, method)(argument)
+    return query.limit(LIMIT)
+
+
 @bp.route('/')
 def list_games():
-    games = db.session.query(Game).limit(LIMIT)
+    games = get_games_query()
     return flask.render_template('index.html', entries=games, title='Games')
 
 @bp.route('/popular')
 def list_games_by_popularity():
-    games = db.session.query(Game).order_by(Game.views.desc()).limit(LIMIT)
+    games = get_games_query(order_by=Game.views.desc())
     return flask.render_template('index.html', entries=games, title='Popular')
 
 @bp.route('/recent')
 def list_games_by_date():
-    games = db.session.query(Game).order_by(Game.updated_at.desc()).limit(LIMIT)
+    games = get_games_query(order_by=Game.updated_at.desc())
     return flask.render_template('index.html', entries=games, title='Recent')
